3bb93b7b31a81493db3d3e934a1f6d34d0259b00
Schuyler Meyer <schuybreach@gmail.com>
2021-11-03 23:19:47 -0400

    Added "download resume" to about page

---
+++ hideText.js
@@ -0,0 +1,10 @@
+/*-- hideText.js // Schuyler Meyer // 2021 --*/
+
+function hiddenTextFunc() {
+  var x = document.getElementById("hiddenText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
