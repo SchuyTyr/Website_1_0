83ed1836ae04126db660be2d86701ebcfb1a35a3
Schuyler Meyer <schuybreach@gmail.com>
2021-12-15 21:53:08 -0500

    Test Test

---
+++ footerMain.php
@@ -0,0 +1,62 @@
+<?php
+
+echo '
+	<hr class="rounded">
+
+	<div class="socialBar" style="width: 100%;">
+
+			<div class="socialProps">
+				<a class="link4" href="https://www.instagram.com/schuyler_meyer/" title="Instagram &#64;schuyler_meyer">
+						<img class="socialHover" src="Instagram_3.png" alt="instagram.com/schuyler_meyer">
+				</a>
+			</div>
+
+			<div class="socialProps">
+				<a class="link4" href="https://twitter.com/schuylermeyer24" title="Twitter &#64;schuylermeyer24">
+						<img class="socialHover" src="Twitter_3.png" alt="twitter.com/schuylermeyer24" >
+				</a>
+			</div>
+
+			<div class="socialProps">
+				<a class="link4" href="https://www.flickr.com/photos/schuyler_meyer/albums" title="Flickr &#64; Schuyler Meyer">
+					<!--<button class="imgSocial" title="Flickr &#64; Schuyler Meyer">-->
+						<img class="socialHover" src="Flickr_3.png" alt="flickr.com/photos/schuyler_meyer">
+				</a>
+			</div>
+
+			<div class="socialProps">
+				<a class="link4" href="https://www.linkedin.com/in/schuyler-meyer/" title="LinkedIn &#64; Schuyler Meyer">
+						<img class="socialHover" src="LinkedIn.png" alt="linkedin.com/in/schuyler-meyer">
+				</a>
+			</div>
+
+			<div class="socialProps">
+				<a class="link4" href="https://www.deviantart.com/akatsukirising" title="DeviantArt &#64;AkatsukiRising">
+						<img class="socialHover" src="DeviantArt_3.png" alt="deviantart.com/akatsukirising" >
+				</a>
+			</div>
+
+			<div class="socialProps">
+				<a class="link4" href="mailto:schuybreach@gmail.com" title="Email &#64; schuybreach@gmail.com">
+						<img class="socialHover" src="Email.png" alt="Email: schuybreach@gmail.com">
+				</a>
+			</div>
+	</div>
+
+	<hr class="rounded">
+	<br>
+	<br>
+	<h4 style="text-align:center;color:#8b8e91;font-size:64px;font-family:verdana;margin: 0;" title="Schuyler Meyer">
+		<img class="glowFade" alt="Schuyler Meyer" src="SMLogo2021Color.png" width="250">
+	</h4>
+
+	<p id="p01">&#169; Schuyler Meyer
+		<b>| 2019 &mdash;
+		<script>document.write(new Date().getFullYear())</script>
+		|</b>
+		<a class="link3" href="readmeText.html" target="_self" title="Readme"> <u>README</u> </a>
+	</p>
+
+';
+
+?>
