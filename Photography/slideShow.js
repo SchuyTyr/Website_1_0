d0ab7df1eceac539d26647412f40bae70036874b
Schuyler Meyer <schuybreach@gmail.com>
2022-01-26 19:49:56 -0500

    Updated Photo Slideshows
    Added counter

--- Photography/slideShow.js
+++ Photography/slideShow.js
@@ -1,20 +1,30 @@
 /*-- slideShow.js // Schuyler Meyer // 2021 --*/
 
 var myIndex = 0;
+var slideCnt = 0;
 carousel();
 
 function carousel() {
   var i;
   var x = document.getElementsByClassName("slides");
+  var sc = document.getElementById("slideCounter");
+  var slideTotal = 0;
 
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
 
+  slideTotal = x.length;
+
   myIndex++;
+  slideCnt++;
+
   if (myIndex > x.length) {
-    myIndex = 1
+    myIndex = 1;
+    slideCnt = 1;
   }
+
+  sc.innerHTML = "<br>&mdash;&nbsp;"+ slideCnt +"&nbsp;/&nbsp;"+ slideTotal +"&nbsp;&mdash;";
 
   x[myIndex-1].style.display = "block";
   //setTimeout(carousel, 6000); // Change image every 6 seconds
