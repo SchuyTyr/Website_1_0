1bc29d277f4e445c10fe6e4e807ed3b072a0a351
Schuyler Meyer <schuybreach@gmail.com>
2022-05-14 22:39:03 -0400

    Added some art, testing new "remote" header

---
+++ src/js/header-photo-inner.js
@@ -0,0 +1,112 @@
+// header-photo-inner.js | Schuyler Meyer | 2022 //
+
+
+// -----------------------------------------------------------------------------------------------------
+
+
+let photoHeaderInner = document.getElementById('PhotoHeaderInner');
+
+photoHeaderInner.innerHTML = `
+<div id="mNav" class="menuOverlay">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" title="Close">&times;</a>
+  <div class="menuOverlay-content">
+
+    <div class="navbar">
+      <div class="subNav">
+        <button class="subNavBtn">Home</button>
+        <div class="subNav-content">
+          <a href="../index.html" title="Home, Main">Home</a>
+          <a href="../photo.html" title="Home, Photos">Photos</a>
+        </div>
+      </div>
+
+      <div class="subNav">
+        <button class="subNavBtn" onclick="void(0)">Sports</button>
+        <div class="subNav-content" onclick="void(0)">
+          <a href="Baseball.html" title="Baseball">Baseball</a>
+          <a href="Basketball.html" title="Basketball">Basketball</a>
+          <a href="Equestrian.html" title="Equestrian">Equestrian</a>
+          <a href="Football.html" title="Football">Football</a>
+          <a href="Golf.html" title="Golf">Golf</a>
+          <a href="MHockey.html" title="Hockey, Men's">Hockey, Men's</a>
+          <a href="WHockey.html" title="Hockey, Women's">Hockey, Women's</a>
+          <a href="Lacrosse.html" title="Lacrosse">Lacrosse</a>
+          <a href="MountainBiking.html" title="Mountain Biking">Mountain Biking</a>
+          <a href="Soccer.html" title="Soccer">Soccer</a>
+          <a href="Softball.html" title="Softball">Softball</a>
+          <a href="SwimDive.html" title="Swimming and Diving">Swimming and Diving</a>
+          <a href="XCountry.html" title="Cross Country">Cross Country</a>
+        </div>
+      </div>
+
+      <div class="subNav">
+        <button class="subNavBtn">Nature</button>
+        <div class="subNav-content">
+          <a href="Animals.html" title="Animals">Animals</a>
+          <a href="Landscape.html" title="Landscape">Landscape</a>
+          <a href="Macro.html" title="Macro">Macro</a>
+        </div>
+      </div>
+
+      <div class="subNav">
+        <button class="subNavBtn" onclick="void(0)">People</button>
+        <div class="subNav-content" onclick="void(0)">
+          <a href="Concerts.html" title="Concerts">Concerts</a>
+          <a href="Cosplay.html" title="Cosplay">Cosplay</a>
+          <a href="Events.html" title="Events">Events</a>
+          <a href="Portraits.html" title="Portraits">Portraits</a>
+        </div>
+      </div>
+
+      <div class="subNav">
+        <button class="subNavBtn">Still Life</button>
+        <div class="subNav-content">
+          <a href="Artwork.html" title="Artwork">Artwork</a>
+          <a href="Miniatures.html" title="Miniatures">Miniatures</a>
+          <a href="StillLife.html" title="Still Life">Still Life</a>
+        </div>
+      </div>
+  </div>
+
+  </div>
+</div>
+
+
+<!-- __________________________________________________________________________ -->
+
+
+<h1 style="color:#8b8e91;" title="Schuyler Meyer">
+  <a href="../index.html">
+    <img alt="Schuyler Meyer" class="imgLogo" src="../src/img/SM_Sign_Photo-blur.png">
+  </a>
+</h1>
+
+<hr class="rounded">
+
+<div class="buttonAbout">
+  <a href="../index.html" class="aButton">
+    <button class="pageButton" title="Home">
+      <div class="pageButtonTxt">&nbsp;Home&nbsp;</div>
+    </button>
+  </a>
+  <button class="pageButton" onclick="openNav()" title="Click to Open the Photo Menu">
+    <div class="pageButtonTxt">&nbsp;<span style="font-size: 20px;">&#9776;</span>&nbsp;Photos&nbsp;</div>
+  </button>
+  <a href="../aboutSM.html" class="aButton">
+    <button class="pageButton hideButton" title="About the Schuy">
+      <div class="pageButtonTxt">&nbsp;About&nbsp;</div>
+    </button>
+  </a>
+  <a href="../contact.html" class="aButton">
+    <button class="pageButton hideButton" title="Coming Soon!">
+      <div class="pageButtonTxt">&nbsp;Contact&nbsp;</div>
+    </button>
+  </a>
+  <a href="https://schuybox.wordpress.com/" class="aButton" target="_blank">
+    <button class="pageButton hideButton" title="To the SchuyBox!">
+      <div class="pageButtonTxt">&nbsp;Blog&nbsp;</div>
+    </button>
+  </a>
+</div>
+
+<hr class="rounded">`;
