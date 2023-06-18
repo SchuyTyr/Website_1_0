c4538e6dc8c5e2635c92b57f6c45db929a5da3cf
Schuyler Meyer <schuybreach@gmail.com>
2023-06-18 17:25:06 -0400

    added date to homepage

---
+++ src/js/dateANDtime.js
@@ -0,0 +1,21 @@
+/*-- dateANDtime.js // Schuyler Meyer // 2023 --*/
+
+const datef = new Date();
+
+//const year = datef.getFullYear();
+//const month = datef.getMonth() + 1; // 0 = Jan, 11 = Dec
+//const day = datef.getDate();
+//const weekDay = datef.getDay().toLocaleString(); // 0 = Sunday, 6 = Saturday
+
+const options = {
+    weekday: "long", month: "long", year: "numeric", day: "numeric"};
+
+//const curDate = weekDay + ", " + month + " " + day + ", " + year;
+
+console.log(new Intl.DateTimeFormat(window.navigator.language, options).format(datef));
+console.log(window.navigator.language);
+console.log(navigator.language);
+
+let writeDate = document.getElementById('date');
+
+writeDate.innerHTML = "&nbsp;" + new Intl.DateTimeFormat(window.navigator.language, options).format(datef) + "&nbsp;";
