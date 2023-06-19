b0fd93245ce384b8456c0a62f0490c640e99823b
Schuyler Meyer <schuybreach@gmail.com>
2023-06-19 02:13:39 -0400

    Added random phrases and languages to the bottom

--- src/js/dateANDtime.js
+++ src/js/dateANDtime.js
@@ -1,4 +1,4 @@
-/*-- dateANDtime.js // Schuyler Meyer // 2023 --*/
+﻿/*-- dateANDtime.js // Schuyler Meyer // 2023 --*/
 
 const datef = new Date();
 
@@ -12,12 +12,35 @@ const options = {
 
 //const curDate = weekDay + ", " + month + " " + day + ", " + year;
 
-console.log(new Intl.DateTimeFormat(window.navigator.language, options).format(datef));
-console.log(window.navigator.language);
+//console.log(new Intl.DateTimeFormat(window.navigator.language, options).format(datef));
+console.log(navigator.language + ", " + new Intl.DateTimeFormat(navigator.language, options).format(datef));
+//console.log(window.navigator.language);
 //console.log(navigator.language);
 
 let writeDate = document.getElementById('date');
 let writeDateInv = document.getElementById('dateInverse');
 
-writeDate.innerHTML = "&nbsp;&nbsp;" + new Intl.DateTimeFormat(window.navigator.language, options).format(datef) + "&nbsp;&nbsp;";
-writeDateInv.innerHTML = "&nbsp;&nbsp;&nbsp;Hello There Friend!&nbsp;&nbsp;&nbsp;";
+const phrases = [
+    "Hello There Friends!",
+    "Welcome to the Schuy!",
+    "こんにちは / Konnichiwa", // Japanese
+    "空へようこそ", // Sora e youkoso, Japanese
+    "Bienvenida al cielo", // Spanish
+    "Willkommen im Himmel", // German
+    "欢迎来到天空", // Huānyíng lái dào tiānkōng, Chinese, simplified
+    "하늘에 오신 것을 환영합니다", // haneul-e osin geos-eul hwan-yeonghabnida, Korean
+    "fáilte go dtí an spéir", // Irish
+    "fàilte do'n adhar", // Scots Gaelic
+    "välkommen till himlen", // Swedish
+    "tervetuloa taivaaseen", // Finnish
+    "velkommen til himmelen", // Norwegian
+    "bienvenue dans le ciel", // French
+    "vítejte na obloze" // Czech
+];
+
+let pCounter = phrases[Math.floor(Math.random() * phrases.length)];
+
+//writeDate.innerHTML = "&nbsp;&nbsp;" + new Intl.DateTimeFormat(window.navigator.language, options).format(datef) + "&nbsp;&nbsp;";
+writeDate.innerHTML = "&nbsp;&nbsp;" + new Intl.DateTimeFormat(navigator.language, options).format(datef) + "&nbsp;&nbsp;";
+writeDateInv.innerHTML = "&nbsp;&nbsp;&nbsp;" + pCounter + "&nbsp;&nbsp;&nbsp;";
+
