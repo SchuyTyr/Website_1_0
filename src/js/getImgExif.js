f0476408ac2644ada50fc268ea6cb5ba7dd017fd
Schuyler Meyer <schuybreach@gmail.com>
2023-07-04 01:29:10 -0400

    updated exif script

--- src/js/getImgExif.js
+++ src/js/getImgExif.js
@@ -7,7 +7,7 @@ window.onload = getExif;
 function getExif() {
     var img1 = document.getElementById("img1");
     EXIF.getData(img1, function () {
-        var imgTitle = EXIF.getTag(this, "Title");
+        var imgTitle = EXIF.getTag(this, "ImageDescription");
 
         img1.title = imgTitle;
     });
