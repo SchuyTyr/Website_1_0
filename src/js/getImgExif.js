bb18b7733f14fd8c8fc885c0821001de7b70d1b8
Schuyler Meyer <schuybreach@gmail.com>
2023-07-04 01:23:35 -0400

    Updated baseball page and cors testing

---
+++ src/js/getImgExif.js
@@ -0,0 +1,14 @@
+﻿/*-- getImgExif.js // Schuyler Meyer // 2023 --*/
+
+//import * as EXIF from "exif.js"; 
+
+window.onload = getExif;
+
+function getExif() {
+    var img1 = document.getElementById("img1");
+    EXIF.getData(img1, function () {
+        var imgTitle = EXIF.getTag(this, "Title");
+
+        img1.title = imgTitle;
+    });
+}
