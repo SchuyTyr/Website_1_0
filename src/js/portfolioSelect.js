18071506223156f685514eb9350e0717413cc997
Schuyler Meyer <schuybreach@gmail.com>
2023-08-12 22:04:56 -0400

    updated portfolio css and got rid of bootstrap

--- src/js/portfolioSelect.js
+++ src/js/portfolioSelect.js
@@ -13,7 +13,7 @@ const html = document.documentElement;
 var text = document.getElementById("myTexT").innerHTML;
 var texts = document.getElementsByClassName("myText");
 
-var vid = document.getElementById("vcontainer");
+var vid = document.getElementById("vcontainerPort");
 
 var descr = [
 	document.getElementsByClassName("myText")[0].innerHTML,
@@ -35,7 +35,7 @@ var descr = [
 	document.getElementsByClassName("myText")[16].innerHTML
 ];
 
-var vaText = document.getElementById("bodyText");
+var vaText = document.getElementById("bodyTextPort");
 
 var noScroll = false;
 
@@ -65,61 +65,61 @@ for (var i = 0; i < images.length; i++) {
 		if(this.getAttribute('id') == 'myImg1'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[0];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/b8JPz9hyxjk?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/b8JPz9hyxjk?rel=0"></iframe>';
 		}
 		// CarrotsVR
 		else if(this.getAttribute('id') == 'myImg2'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[1];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/KSGVi-_yQFo?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/KSGVi-_yQFo?rel=0"></iframe>';
 		}
 		// VR Hockey
 		else if(this.getAttribute('id') == 'myImg4'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[2];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/YdjYonT1VPY?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/YdjYonT1VPY?rel=0"></iframe>';
 		}
 		// Konbini
 		else if(this.getAttribute('id') == 'myImg5'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[3];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/YvHBGxTTjos?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/YvHBGxTTjos?rel=0"></iframe>';
 		}
 		// The Cube, Evolved
 		else if(this.getAttribute('id') == 'myImg6'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[4];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/uiQBXOmWC2M?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/uiQBXOmWC2M?rel=0"></iframe>';
 		}
 		// Untitled Animation Tests
 		else if(this.getAttribute('id') == 'myImg11'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[5];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/w2kJaepBS7k?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/w2kJaepBS7k?rel=0"></iframe>';
 		}
 		// The Spooky and Mysterious Box
 		else if(this.getAttribute('id') == 'myImg7'){
 			modalImg.src = this.src;
 			vaText.innerHTML = descr[6];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/JpfNoArEEHw?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/JpfNoArEEHw?rel=0"></iframe>';
 		}
 		// DA300 Animation
 		else if(this.getAttribute('id') == 'myImg13'){
 			/*modalImg.src = '';
 			vaText.innerHTML = descr[15];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/lbHEsqueyiY"></iframe>';*/
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/lbHEsqueyiY"></iframe>';*/
 			/*modalImg.src = this.src;
 			vaText.innerHTML = descr[10];
 			vid.innerHTML = '';*/
 			modalImg.src = '';
 			vaText.innerHTML = descr[7];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/mHvkkD9hmiI?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/mHvkkD9hmiI?rel=0"></iframe>';
 		}
 		// Zaim
 		else if(this.getAttribute('id') == 'myImg17'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[8];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/lbHEsqueyiY?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/lbHEsqueyiY?rel=0"></iframe>';
 		}
 		// Space Ship
 		else if(this.getAttribute('id') == 'myImg8'){
@@ -128,31 +128,31 @@ for (var i = 0; i < images.length; i++) {
 			vid.innerHTML = '';
 			/*modalImg.src = '';
 			vaText.innerHTML = descr[7];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/HvVJKZyXrKc"></iframe>';*/
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/HvVJKZyXrKc"></iframe>';*/
 		}
 		// SOB
 		else if(this.getAttribute('id') == 'myImg3'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[10];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/TyFg2lPMnGQ?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/TyFg2lPMnGQ?rel=0"></iframe>';
 		}
 		// Swarm
 		else if(this.getAttribute('id') == 'myImg10'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[11];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/tDnwA2Lajkw?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/tDnwA2Lajkw?rel=0"></iframe>';
 		}
 		// CGK Promo
 		else if(this.getAttribute('id') == 'myImg16'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[12];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/teKWmAu9UxU?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/teKWmAu9UxU?rel=0"></iframe>';
 		}
 		// Hockey Cards
 		else if(this.getAttribute('id') == 'myImg9'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[13];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/KWw_-XWKMek?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/KWw_-XWKMek?rel=0"></iframe>';
 		}
 		// Board Game
 		else if(this.getAttribute('id') == 'myImg15'){
@@ -164,19 +164,19 @@ for (var i = 0; i < images.length; i++) {
 		else if(this.getAttribute('id') == 'myImg14'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[15];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/HvVJKZyXrKc?rel=0"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/HvVJKZyXrKc?rel=0"></iframe>';
 		}
 		// Monuments
 		/*else if(this.getAttribute('id') == 'myImg11'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[3];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://player.vimeo.com/video/273553184"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://player.vimeo.com/video/273553184"></iframe>';
 		}*/
 		// The Future of Sports Photography
 		/*else if(this.getAttribute('id') == 'myImg12'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[11];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/rictK6On9B4"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/rictK6On9B4"></iframe>';
 		}*/
 		// Artstation
 		else if(this.getAttribute('id') == 'myImg12'){
@@ -188,7 +188,7 @@ for (var i = 0; i < images.length; i++) {
 		/*else if(this.getAttribute('id') == 'myImg14'){
 			modalImg.src = '';
 			vaText.innerHTML = descr[10];
-			vid.innerHTML = '<iframe allowfullscreen class="video" frameborder="0" src="https://www.youtube.com/embed/jByZKsF_9uc"></iframe>';
+			vid.innerHTML = '<iframe allowfullscreen class="videoPort" frameborder="0" src="https://www.youtube.com/embed/jByZKsF_9uc"></iframe>';
 		}*/
 		else {
 			modalImg.src = '';
@@ -199,7 +199,7 @@ for (var i = 0; i < images.length; i++) {
 	}
 }
 
-var clseTop = document.getElementsByClassName("closeBtn")[0];
+var clseTop = document.getElementsByClassName("closeBtnPort")[0];
 
 clseTop.onclick = function() {
 	modal.style.display = "none";
@@ -212,7 +212,7 @@ clseTop.onclick = function() {
 	vid.innerHTML = '';
 }
 
-var clseBtm = document.getElementsByClassName("closeBtn")[1];
+var clseBtm = document.getElementsByClassName("closeBtnPort")[1];
 
 clseBtm.onclick = function() {
 	modal.style.display = "none";
