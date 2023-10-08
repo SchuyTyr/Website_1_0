823a37dba6a3a42c440f4a23c5b16a8636e277f8
Schuyler Meyer <schuybreach@gmail.com>
2023-10-07 22:34:38 -0400

    added unfinished side projects

---
+++ src/js/entryType.js
@@ -0,0 +1,38 @@
+﻿/*-- entryType.js // Schuyler Meyer // 2023 --*/
+
+//var bookFieldset = document.getElementById(bookFieldset);
+//var movieFieldset = document.getElementById(movieFieldset);
+//var musicFieldset = document.getElementById(musicFieldset);
+
+function disableField() {
+
+    //var mediaTypeForm = document.getElementById(mediaTypeForm);
+    //var mediaTypeSelect = document.getElementById(mediaTypeSelect);
+
+    var mediaType = document.forms["mediaTypeForm"]["mediaTypeSelect"].value;
+
+    if (mediaType === "bookFieldset") {
+        document.getElementById("bookFieldset").disabled = false;
+        document.getElementById("movieFieldset").disabled = true;
+        document.getElementById("musicFieldset").disabled = true;
+
+    } else if (mediaType === "movieFieldset") {
+        document.getElementById("bookFieldset").disabled = true;
+        document.getElementById("movieFieldset").disabled = false;
+        document.getElementById("musicFieldset").disabled = true;
+
+    } else if (mediaType === "musicFieldset") {
+        document.getElementById("bookFieldset").disabled = true;
+        document.getElementById("movieFieldset").disabled = true;
+        document.getElementById("musicFieldset").disabled = false;
+
+    } else {
+        document.getElementById("bookFieldset").disabled = true;
+        document.getElementById("movieFieldset").disabled = true;
+        document.getElementById("musicFieldset").disabled = true;
+    }
+}
+
+document.getElementById("bookFieldset").disabled = true;
+document.getElementById("movieFieldset").disabled = true;
+document.getElementById("musicFieldset").disabled = true;
---
+++ src/js/photoBeforeAfter.js
@@ -0,0 +1,14 @@
+﻿/*-- photoBeforeAfter.js // Schuyler Meyer // 2023 --*/
+
+
+var divisor = document.getElementById("divisor"),
+	slider = document.getElementById("slider"),
+	divisor2 = document.getElementById("divisor2"),
+	slider2 = document.getElementById("slider2");
+
+function moveDivisor() {
+	divisor.style.width = slider.value + "%";
+}
+function moveDivisor2() {
+	divisor2.style.width = slider2.value + "%";
+}
