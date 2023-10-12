e3f47c985f753221b1f8435f332610b1e8ca93a2
Schuyler Meyer <schuybreach@gmail.com>
2023-10-12 17:04:55 -0400

    Stupid CORS again

---
+++ src/js/kadane_Algo.js
@@ -0,0 +1,19 @@
+﻿/*-- kadane_Algo.js // Schuyler Meyer // 2023 --*/
+
+// The runtime complexity of Kadane's algorithm is O(n) and its space complexity is O(1).
+
+export function max_subarray(numbers) {
+
+    // Find the largest sum of any contiguous subarray.
+    var best_sum = - Infinity
+    var current_sum = 0
+
+    for (var x in numbers) {
+        current_sum = Math.max(x, current_sum + x)
+        best_sum = Math.max(best_sum, current_sum)
+    }
+
+    console.log("best_sum: " + best_sum);
+    return best_sum
+}
+
---
+++ src/js/testing_Algos.js
@@ -0,0 +1,22 @@
+﻿/*-- testing_Algos.js // Schuyler Meyer // 2023 --*/
+
+// Kadane's Algo
+
+import { max_subarray } from "./kadane_Algo.js";
+
+function kadaneAlgo() {
+    var numForAlgo = document.getElementById(numForAlgo);
+    console.log("numForAlgo: " + numForAlgo);
+
+    var output = document.getElementById(output);
+
+    var separatedArray = numForAlgo.split(', ');
+    console.log("separatedArray: " + separatedArray);
+
+    var numbers = Number(separatedArray);
+    console.log("numbers: " + numbers);
+
+    output.innerHTML = max_subarray(numbers);
+
+    
+}
